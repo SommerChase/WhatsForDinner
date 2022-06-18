50383bed5b178a169aedb257e71db1ba6f61b497
Chase Sommer <robertchasesommer@gmail.com>
2022-06-18 18:32:07 -0400

    Added second tutorial.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 """
 https://flask.palletsprojects.com/en/2.1.x/quickstart/
+https://www.digitalocean.com/community/tutorials/how-to-create-your-first-web-application-using-flask-and-python-3
 
 Initializing the Flask app. I was unable to get it to work with a different name
 than app.py. FLASK_ENV = "name" wasn't working, so I used a shortcut by renaming 
